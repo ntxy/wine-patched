wine-mirror/wine

commit cad4badbcf25992e0c61521aa15e639c2611f5d6

wined3d: Get rid of wined3d_select_blitter().
Instead, chain the blitters themselves. This also fixes the issue that
currently only a single blitter can store extra data in the "blit_priv" field
of struct wined3d_device.

Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/wined3d/device.c
@@ -979,7 +979,7 @@ static void wined3d_device_delete_opengl_contexts_cs(void *object)
     }
 
     context = context_acquire(device, NULL, 0);
-    device->blitter->free_private(device);
+    device->blitter->ops->blitter_destroy(device->blitter, context);
     device->shader_backend->shader_free_private(device);
     destroy_dummy_textures(device, context);
     destroy_default_samplers(device, context);
@@ -1014,12 +1014,15 @@ static void wined3d_device_create_primary_opengl_context_cs(void *object)
         return;
     }
 
-    if (FAILED(hr = device->blitter->alloc_private(device)))
+    if (!(device->blitter = wined3d_cpu_blitter_create()))
     {
-        ERR("Failed to allocate blitter private data, hr %#x.\n", hr);
+        ERR("Failed to create CPU blitter.\n");
         device->shader_backend->shader_free_private(device);
         return;
     }
+    wined3d_ffp_blitter_create(&device->blitter, &device->adapter->gl_info);
+    wined3d_arbfp_blitter_create(&device->blitter, device);
+    wined3d_fbo_blitter_create(&device->blitter, &device->adapter->gl_info);
 
     swapchain = device->swapchains[0];
     target = swapchain->back_buffers ? swapchain->back_buffers[0] : swapchain->front_buffer;
@@ -4144,9 +4147,7 @@ HRESULT CDECL wined3d_device_clear_rendertarget_view(struct wined3d_device *devi
         struct wined3d_rendertarget_view *view, const RECT *rect, DWORD flags,
         const struct wined3d_color *color, float depth, DWORD stencil)
 {
-    const struct wined3d_blitter_ops *blitter;
     struct wined3d_resource *resource;
-    enum wined3d_blit_op blit_op;
     RECT r;
 
     TRACE("device %p, view %p, rect %s, flags %#x, color %s, depth %.8e, stencil %u.\n",
@@ -4184,19 +4185,7 @@ HRESULT CDECL wined3d_device_clear_rendertarget_view(struct wined3d_device *devi
             return hr;
     }
 
-    if (flags & WINED3DCLEAR_TARGET)
-        blit_op = WINED3D_BLIT_OP_COLOR_FILL;
-    else
-        blit_op = WINED3D_BLIT_OP_DEPTH_FILL;
-
-    if (!(blitter = wined3d_select_blitter(&device->adapter->gl_info, &device->adapter->d3d_info,
-            blit_op, NULL, 0, 0, NULL, rect, resource->usage, resource->pool, resource->format)))
-    {
-        FIXME("No blitter is capable of performing the requested fill operation.\n");
-        return WINED3DERR_INVALIDCALL;
-    }
-
-    blitter->blitter_clear(device, view, rect, flags, color, depth, stencil);
+    device->blitter->ops->blitter_clear(device->blitter, device, view, rect, flags, color, depth, stencil);
 
     return WINED3D_OK;
 }
@@ -4980,8 +4969,6 @@ HRESULT device_init(struct wined3d_device *device, struct wined3d *wined3d,
         wined3d_decref(device->wined3d);
         return hr;
     }
-
-    device->blitter = adapter->blitter;
 
     state_init(&device->state, &device->fb, &adapter->gl_info,
             &adapter->d3d_info, WINED3D_STATE_INIT_DEFAULT);

dlls/wined3d/arb_program_shader.c
@@ -6879,8 +6879,9 @@ struct arbfp_blit_desc
 #define ARBFP_BLIT_PARAM_COLOR_KEY_LOW 1
 #define ARBFP_BLIT_PARAM_COLOR_KEY_HIGH 2
 
-struct arbfp_blit_priv
+struct wined3d_arbfp_blitter
 {
+    struct wined3d_blitter blitter;
     struct wine_rb_tree shaders;
     GLuint palette_texture;
 };
@@ -6894,44 +6895,39 @@ static int arbfp_blit_type_compare(const void *key, const struct wine_rb_entry *
 }
 
 /* Context activation is done by the caller. */
-static void arbfp_free_blit_shader(struct wine_rb_entry *entry, void *context)
+static void arbfp_free_blit_shader(struct wine_rb_entry *entry, void *ctx)
 {
-    const struct wined3d_gl_info *gl_info = context;
     struct arbfp_blit_desc *entry_arb = WINE_RB_ENTRY_VALUE(entry, struct arbfp_blit_desc, entry);
+    const struct wined3d_gl_info *gl_info;
+    struct wined3d_context *context;
+
+    context = ctx;
+    gl_info = context->gl_info;
 
     GL_EXTCALL(glDeleteProgramsARB(1, &entry_arb->shader));
     checkGLcall("glDeleteProgramsARB(1, &entry_arb->shader)");
     HeapFree(GetProcessHeap(), 0, entry_arb);
 }
 
-static HRESULT arbfp_blit_alloc(struct wined3d_device *device)
-{
-    struct arbfp_blit_priv *priv;
-
-    if (!(priv = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(*priv))))
-        return E_OUTOFMEMORY;
-
-    wine_rb_init(&priv->shaders, arbfp_blit_type_compare);
-
-    device->blit_priv = priv;
-
-    return WINED3D_OK;
-}
-
 /* Context activation is done by the caller. */
-static void arbfp_blit_free(struct wined3d_device *device)
+static void arbfp_blitter_destroy(struct wined3d_blitter *blitter, struct wined3d_context *context)
 {
-    const struct wined3d_gl_info *gl_info = &device->adapter->gl_info;
-    struct arbfp_blit_priv *priv = device->blit_priv;
+    const struct wined3d_gl_info *gl_info = context->gl_info;
+    struct wined3d_arbfp_blitter *arbfp_blitter;
+    struct wined3d_blitter *next;
 
-    wine_rb_destroy(&priv->shaders, arbfp_free_blit_shader, &device->adapter->gl_info);
+    if ((next = blitter->next))
+        next->ops->blitter_destroy(next, context);
+
+    arbfp_blitter = CONTAINING_RECORD(blitter, struct wined3d_arbfp_blitter, blitter);
+
+    wine_rb_destroy(&arbfp_blitter->shaders, arbfp_free_blit_shader, context);
     checkGLcall("Delete blit programs");
 
-    if (priv->palette_texture)
-        gl_info->gl_ops.gl.p_glDeleteTextures(1, &priv->palette_texture);
+    if (arbfp_blitter->palette_texture)
+        gl_info->gl_ops.gl.p_glDeleteTextures(1, &arbfp_blitter->palette_texture);
 
-    HeapFree(GetProcessHeap(), 0, device->blit_priv);
-    device->blit_priv = NULL;
+    HeapFree(GetProcessHeap(), 0, arbfp_blitter);
 }
 
 static BOOL gen_planar_yuv_read(struct wined3d_string_buffer *buffer, const struct arbfp_blit_type *type,
@@ -7358,18 +7354,17 @@ static GLuint gen_p8_shader(const struct wined3d_gl_info *gl_info, const struct 
 }
 
 /* Context activation is done by the caller. */
-static void upload_palette(const struct wined3d_texture *texture, struct wined3d_context *context)
+static void upload_palette(struct wined3d_arbfp_blitter *blitter,
+        const struct wined3d_texture *texture, struct wined3d_context *context)
 {
     const struct wined3d_palette *palette = texture->swapchain ? texture->swapchain->palette : NULL;
-    struct wined3d_device *device = texture->resource.device;
     const struct wined3d_gl_info *gl_info = context->gl_info;
-    struct arbfp_blit_priv *priv = device->blit_priv;
 
-    if (!priv->palette_texture)
-        gl_info->gl_ops.gl.p_glGenTextures(1, &priv->palette_texture);
+    if (!blitter->palette_texture)
+        gl_info->gl_ops.gl.p_glGenTextures(1, &blitter->palette_texture);
 
     GL_EXTCALL(glActiveTexture(GL_TEXTURE1));
-    gl_info->gl_ops.gl.p_glBindTexture(GL_TEXTURE_1D, priv->palette_texture);
+    gl_info->gl_ops.gl.p_glBindTexture(GL_TEXTURE_1D, blitter->palette_texture);
 
     gl_info->gl_ops.gl.p_glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
 
@@ -7573,11 +7568,10 @@ static GLuint arbfp_gen_plain_shader(const struct wined3d_gl_info *gl_info, cons
 }
 
 /* Context activation is done by the caller. */
-static HRESULT arbfp_blit_set(void *blit_priv, struct wined3d_context *context, const struct wined3d_surface *surface,
-        const struct wined3d_color_key *color_key)
+static HRESULT arbfp_blit_set(struct wined3d_arbfp_blitter *blitter, struct wined3d_context *context,
+        const struct wined3d_surface *surface, const struct wined3d_color_key *color_key)
 {
     const struct wined3d_texture *texture = surface->container;
-    struct arbfp_blit_priv *priv = blit_priv;
     enum complex_fixup fixup;
     const struct wined3d_gl_info *gl_info = context->gl_info;
     struct wine_rb_entry *entry;
@@ -7627,8 +7621,7 @@ static HRESULT arbfp_blit_set(void *blit_priv, struct wined3d_context *context, 
     type.use_color_key = !!color_key;
     type.padding = 0;
 
-    entry = wine_rb_get(&priv->shaders, &type);
-    if (entry)
+    if ((entry = wine_rb_get(&blitter->shaders, &type)))
     {
         desc = WINE_RB_ENTRY_VALUE(entry, struct arbfp_blit_desc, entry);
         shader = desc->shader;
@@ -7667,7 +7660,7 @@ static HRESULT arbfp_blit_set(void *blit_priv, struct wined3d_context *context, 
 
         desc->type = type;
         desc->shader = shader;
-        if (wine_rb_put(&priv->shaders, &desc->type, &desc->entry) == -1)
+        if (wine_rb_put(&blitter->shaders, &desc->type, &desc->entry) == -1)
         {
 err_out:
             ERR("Out of memory\n");
@@ -7681,7 +7674,7 @@ err_out:
     }
 
     if (fixup == COMPLEX_FIXUP_P8)
-        upload_palette(texture, context);
+        upload_palette(blitter, texture, context);
 
     gl_info->gl_ops.gl.p_glEnable(GL_FRAGMENT_PROGRAM_ARB);
     checkGLcall("glEnable(GL_FRAGMENT_PROGRAM_ARB)");
@@ -7711,8 +7704,8 @@ static void arbfp_blit_unset(const struct wined3d_gl_info *gl_info)
 
 static BOOL arbfp_blit_supported(const struct wined3d_gl_info *gl_info,
         const struct wined3d_d3d_info *d3d_info, enum wined3d_blit_op blit_op,
-        const RECT *src_rect, DWORD src_usage, enum wined3d_pool src_pool, const struct wined3d_format *src_format,
-        const RECT *dst_rect, DWORD dst_usage, enum wined3d_pool dst_pool, const struct wined3d_format *dst_format)
+        enum wined3d_pool src_pool, const struct wined3d_format *src_format,
+        enum wined3d_pool dst_pool, const struct wined3d_format *dst_format)
 {
     enum complex_fixup src_fixup;
     BOOL decompress;
@@ -7786,15 +7779,30 @@ static BOOL arbfp_blit_supported(const struct wined3d_gl_info *gl_info,
     }
 }
 
-static void arbfp_blit_surface(struct wined3d_device *device, enum wined3d_blit_op op, struct wined3d_context *context,
-        struct wined3d_surface *src_surface, DWORD src_location, const RECT *src_rect,
-        struct wined3d_surface *dst_surface, DWORD dst_location, const RECT *dst_rect,
+static void arbfp_blitter_blit(struct wined3d_blitter *blitter, enum wined3d_blit_op op,
+        struct wined3d_context *context, struct wined3d_surface *src_surface, DWORD src_location,
+        const RECT *src_rect, struct wined3d_surface *dst_surface, DWORD dst_location, const RECT *dst_rect,
         const struct wined3d_color_key *color_key, enum wined3d_texture_filter_type filter)
 {
     struct wined3d_texture *src_texture = src_surface->container;
     struct wined3d_texture *dst_texture = dst_surface->container;
+    struct wined3d_device *device = dst_texture->resource.device;
+    struct wined3d_arbfp_blitter *arbfp_blitter;
     struct wined3d_color_key alpha_test_key;
+    struct wined3d_blitter *next;
     RECT s, d;
+
+    if (!arbfp_blit_supported(&device->adapter->gl_info, &device->adapter->d3d_info, op,
+            src_texture->resource.pool, src_texture->resource.format,
+            dst_texture->resource.pool, dst_texture->resource.format))
+    {
+        if ((next = blitter->next))
+            next->ops->blitter_blit(next, op, context, src_surface, src_location,
+                    src_rect, dst_surface, dst_location, dst_rect, color_key, filter);
+        return;
+    }
+
+    arbfp_blitter = CONTAINING_RECORD(blitter, struct wined3d_arbfp_blitter, blitter);
 
     /* Now load the surface */
     if (wined3d_settings.offscreen_rendering_mode != ORM_FBO
@@ -7855,7 +7863,7 @@ static void arbfp_blit_surface(struct wined3d_device *device, enum wined3d_blit_
         color_key = &alpha_test_key;
     }
 
-    arbfp_blit_set(device->blit_priv, context, src_surface, color_key);
+    arbfp_blit_set(arbfp_blitter, context, src_surface, color_key);
 
     /* Draw a textured quad */
     draw_textured_quad(src_surface, context, src_rect, dst_rect, filter);
@@ -7868,17 +7876,46 @@ static void arbfp_blit_surface(struct wined3d_device *device, enum wined3d_blit_
         context->gl_info->gl_ops.gl.p_glFlush(); /* Flush to ensure ordering across contexts. */
 }
 
-static void arbfp_blit_clear(struct wined3d_device *device, struct wined3d_rendertarget_view *view,
-        const RECT *rect, DWORD flags, const struct wined3d_color *colour, float depth, DWORD stencil)
+static void arbfp_blitter_clear(struct wined3d_blitter *blitter, struct wined3d_device *device,
+        struct wined3d_rendertarget_view *view, const RECT *rect, DWORD flags,
+        const struct wined3d_color *colour, float depth, DWORD stencil)
 {
-    ERR("This blitter does not implement clears.\n");
+    struct wined3d_blitter *next;
+
+    if ((next = blitter->next))
+        next->ops->blitter_clear(next, device, view, rect, flags, colour, depth, stencil);
 }
 
-const struct wined3d_blitter_ops arbfp_blit =
+static const struct wined3d_blitter_ops arbfp_blitter_ops =
 {
-    arbfp_blit_alloc,
-    arbfp_blit_free,
-    arbfp_blit_supported,
-    arbfp_blit_clear,
-    arbfp_blit_surface,
+    arbfp_blitter_destroy,
+    arbfp_blitter_clear,
+    arbfp_blitter_blit,
 };
+
+void wined3d_arbfp_blitter_create(struct wined3d_blitter **next, const struct wined3d_device *device)
+{
+    const struct wined3d_gl_info *gl_info = &device->adapter->gl_info;
+    struct wined3d_arbfp_blitter *blitter;
+
+    if (device->shader_backend != &arb_program_shader_backend
+            && device->shader_backend != &glsl_shader_backend)
+        return;
+
+    if (!gl_info->supported[ARB_FRAGMENT_PROGRAM])
+        return;
+
+    if (!(blitter = HeapAlloc(GetProcessHeap(), 0, sizeof(*blitter))))
+    {
+        ERR("Failed to allocate blitter.\n");
+        return;
+    }
+
+    TRACE("Created blitter %p.\n", blitter);
+
+    blitter->blitter.ops = &arbfp_blitter_ops;
+    blitter->blitter.next = *next;
+    wine_rb_init(&blitter->shaders, arbfp_blit_type_compare);
+    blitter->palette_texture = 0;
+    *next = &blitter->blitter;
+}

dlls/wined3d/directx.c
@@ -2599,16 +2599,6 @@ static const struct wined3d_shader_backend_ops *select_shader_backend(const stru
     return &none_shader_backend;
 }
 
-static const struct wined3d_blitter_ops *select_blit_implementation(const struct wined3d_gl_info *gl_info,
-        const struct wined3d_shader_backend_ops *shader_backend_ops)
-{
-    if ((shader_backend_ops == &glsl_shader_backend
-            || shader_backend_ops == &arb_program_shader_backend)
-            && gl_info->supported[ARB_FRAGMENT_PROGRAM])
-        return &arbfp_blit;
-    return &ffp_blit;
-}
-
 static void parse_extension_string(struct wined3d_gl_info *gl_info, const char *extensions,
         const struct wined3d_extension_map *map, UINT entry_count)
 {
@@ -4169,7 +4159,6 @@ static BOOL wined3d_adapter_init_gl_caps(struct wined3d_adapter *adapter,
     adapter->shader_backend = select_shader_backend(gl_info);
     adapter->vertex_pipe = select_vertex_implementation(gl_info, adapter->shader_backend);
     adapter->fragment_pipe = select_fragment_implementation(gl_info, adapter->shader_backend);
-    adapter->blitter = select_blit_implementation(gl_info, adapter->shader_backend);
 
     adapter->shader_backend->shader_get_caps(gl_info, &shader_caps);
     adapter->d3d_info.vs_clipping = shader_caps.wined3d_caps & WINED3D_SHADER_CAP_VS_CLIPPING;
@@ -6567,7 +6556,6 @@ static BOOL wined3d_adapter_init_nogl(struct wined3d_adapter *adapter, UINT ordi
     adapter->vertex_pipe = &none_vertex_pipe;
     adapter->fragment_pipe = &none_fragment_pipe;
     adapter->shader_backend = &none_shader_backend;
-    adapter->blitter = &cpu_blit;
 
     display_device.cb = sizeof(display_device);
     EnumDisplayDevicesW(NULL, ordinal, &display_device, 0);

dlls/wined3d/swapchain.c
@@ -306,21 +306,11 @@ static void swapchain_blit(const struct wined3d_swapchain *swapchain,
     struct wined3d_texture *texture = swapchain->back_buffers[0];
     struct wined3d_surface *back_buffer = texture->sub_resources[0].u.surface;
     struct wined3d_device *device = swapchain->device;
-    const struct wined3d_blitter_ops *blitter;
     enum wined3d_texture_filter_type filter;
     DWORD location;
 
     TRACE("swapchain %p, context %p, src_rect %s, dst_rect %s.\n",
             swapchain, context, wine_dbgstr_rect(src_rect), wine_dbgstr_rect(dst_rect));
-
-    if (!(blitter = wined3d_select_blitter(&device->adapter->gl_info,
-            &device->adapter->d3d_info, WINED3D_BLIT_OP_COLOR_BLIT,
-            src_rect, texture->resource.usage, texture->resource.pool, texture->resource.format,
-            dst_rect, texture->resource.usage, texture->resource.pool, texture->resource.format)))
-    {
-        FIXME("No blitter supports the requested blit.\n");
-        return;
-    }
 
     if ((src_rect->right - src_rect->left == dst_rect->right - dst_rect->left
             && src_rect->bottom - src_rect->top == dst_rect->bottom - dst_rect->top)
@@ -334,8 +324,8 @@ static void swapchain_blit(const struct wined3d_swapchain *swapchain,
         location = WINED3D_LOCATION_RB_RESOLVED;
 
     wined3d_texture_validate_location(texture, 0, WINED3D_LOCATION_DRAWABLE);
-    blitter->blit_surface(device, WINED3D_BLIT_OP_COLOR_BLIT, context, back_buffer, location,
-            src_rect, back_buffer, WINED3D_LOCATION_DRAWABLE, dst_rect, NULL, filter);
+    device->blitter->ops->blitter_blit(device->blitter, WINED3D_BLIT_OP_COLOR_BLIT, context, back_buffer,
+            location, src_rect, back_buffer, WINED3D_LOCATION_DRAWABLE, dst_rect, NULL, filter);
     wined3d_texture_invalidate_location(texture, 0, WINED3D_LOCATION_DRAWABLE);
 }
 

dlls/wined3d/utils.c
@@ -5794,37 +5794,6 @@ int wined3d_ffp_vertex_program_key_compare(const void *key, const struct wine_rb
     return memcmp(ka, kb, sizeof(*ka));
 }
 
-const struct wined3d_blitter_ops *wined3d_select_blitter(const struct wined3d_gl_info *gl_info,
-        const struct wined3d_d3d_info *d3d_info, enum wined3d_blit_op blit_op,
-        const RECT *src_rect, DWORD src_usage, enum wined3d_pool src_pool, const struct wined3d_format *src_format,
-        const RECT *dst_rect, DWORD dst_usage, enum wined3d_pool dst_pool, const struct wined3d_format *dst_format)
-{
-    static const struct wined3d_blitter_ops * const blitters[] =
-    {
-        &fbo_blitter_ops,
-        &arbfp_blit,
-        &ffp_blit,
-        &cpu_blit,
-    };
-    unsigned int i;
-
-    TRACE("gl_info %p, d3d_info %p, blit_op %#x, src_rect %s, src_usage %s, src_pool %s, src_format %s, "
-            "dst_rect %s, dst_usage %s, dst_pool %s, dst_format %s.\n", gl_info, d3d_info, blit_op,
-            wine_dbgstr_rect(src_rect), debug_d3dusage(src_usage), debug_d3dpool(src_pool),
-            src_format ? debug_d3dformat(src_format->id) : "(null)", wine_dbgstr_rect(dst_rect),
-            debug_d3dusage(dst_usage), debug_d3dpool(dst_pool), debug_d3dformat(dst_format->id));
-
-    for (i = 0; i < sizeof(blitters) / sizeof(*blitters); ++i)
-    {
-        if (blitters[i]->blit_supported(gl_info, d3d_info, blit_op,
-                src_rect, src_usage, src_pool, src_format,
-                dst_rect, dst_usage, dst_pool, dst_format))
-            return blitters[i];
-    }
-
-    return NULL;
-}
-
 void wined3d_get_draw_rect(const struct wined3d_state *state, RECT *rect)
 {
     const struct wined3d_viewport *vp = &state->viewport;

dlls/wined3d/wined3d_private.h
@@ -1867,33 +1867,33 @@ enum wined3d_blit_op
     WINED3D_BLIT_OP_DEPTH_BLIT,
 };
 
+struct wined3d_blitter
+{
+    const struct wined3d_blitter_ops *ops;
+    struct wined3d_blitter *next;
+};
+
 struct wined3d_blitter_ops
 {
-    HRESULT (*alloc_private)(struct wined3d_device *device);
-    void (*free_private)(struct wined3d_device *device);
-    BOOL (*blit_supported)(const struct wined3d_gl_info *gl_info,
-            const struct wined3d_d3d_info *d3d_info, enum wined3d_blit_op blit_op,
-            const RECT *src_rect, DWORD src_usage, enum wined3d_pool src_pool, const struct wined3d_format *src_format,
-            const RECT *dst_rect, DWORD dst_usage, enum wined3d_pool dst_pool, const struct wined3d_format *dst_format);
-    void (*blitter_clear)(struct wined3d_device *device, struct wined3d_rendertarget_view *view,
-            const RECT *rect, DWORD flags, const struct wined3d_color *colour, float depth, DWORD stencil);
-    void (*blit_surface)(struct wined3d_device *device, enum wined3d_blit_op op, struct wined3d_context *context,
+    void (*blitter_destroy)(struct wined3d_blitter *blitter, struct wined3d_context *context);
+    void (*blitter_clear)(struct wined3d_blitter *blitter, struct wined3d_device *device,
+            struct wined3d_rendertarget_view *view, const RECT *rect, DWORD flags,
+            const struct wined3d_color *colour, float depth, DWORD stencil);
+    void (*blitter_blit)(struct wined3d_blitter *blitter, enum wined3d_blit_op op, struct wined3d_context *context,
             struct wined3d_surface *src_surface, DWORD src_location, const RECT *src_rect,
             struct wined3d_surface *dst_surface, DWORD dst_location, const RECT *dst_rect,
             const struct wined3d_color_key *color_key, enum wined3d_texture_filter_type filter);
 };
 
-extern const struct wined3d_blitter_ops fbo_blitter_ops DECLSPEC_HIDDEN;
-extern const struct wined3d_blitter_ops arbfp_blit DECLSPEC_HIDDEN;
-extern const struct wined3d_blitter_ops ffp_blit DECLSPEC_HIDDEN;
-extern const struct wined3d_blitter_ops cpu_blit DECLSPEC_HIDDEN;
+void wined3d_arbfp_blitter_create(struct wined3d_blitter **next,
+        const struct wined3d_device *device) DECLSPEC_HIDDEN;
+struct wined3d_blitter *wined3d_cpu_blitter_create(void) DECLSPEC_HIDDEN;
+void wined3d_fbo_blitter_create(struct wined3d_blitter **next,
+        const struct wined3d_gl_info *gl_info) DECLSPEC_HIDDEN;
+void wined3d_ffp_blitter_create(struct wined3d_blitter **next,
+        const struct wined3d_gl_info *gl_info) DECLSPEC_HIDDEN;
 
 BOOL wined3d_clip_blit(const RECT *clip_rect, RECT *clipped, RECT *other) DECLSPEC_HIDDEN;
-const struct wined3d_blitter_ops *wined3d_select_blitter(const struct wined3d_gl_info *gl_info,
-        const struct wined3d_d3d_info *d3d_info, enum wined3d_blit_op blit_op,
-        const RECT *src_rect, DWORD src_usage, enum wined3d_pool src_pool, const struct wined3d_format *src_format,
-        const RECT *dst_rect, DWORD dst_usage, enum wined3d_pool dst_pool, const struct wined3d_format *dst_format)
-        DECLSPEC_HIDDEN;
 
 struct wined3d_context *context_acquire(const struct wined3d_device *device,
         struct wined3d_texture *texture, unsigned int sub_resource_idx) DECLSPEC_HIDDEN;
@@ -2361,7 +2361,6 @@ struct wined3d_adapter
     const struct wined3d_vertex_pipe_ops *vertex_pipe;
     const struct fragment_pipeline *fragment_pipe;
     const struct wined3d_shader_backend_ops *shader_backend;
-    const struct wined3d_blitter_ops *blitter;
 };
 
 struct wined3d_caps_gl_ctx
@@ -2624,11 +2623,10 @@ struct wined3d_device
     void *shader_priv;
     void *fragment_priv;
     void *vertex_priv;
-    void *blit_priv;
     struct StateEntry StateTable[STATE_HIGHEST + 1];
     /* Array of functions for states which are handled by more than one pipeline part */
     APPLYSTATEFUNC *multistate_funcs[STATE_HIGHEST + 1];
-    const struct wined3d_blitter_ops *blitter;
+    struct wined3d_blitter *blitter;
 
     BYTE vertexBlendUsed : 1;           /* To avoid needless setting of the blend matrices */
     BYTE bCursorVisible : 1;

ntxy/wine-patched

commit b0f8d2651639ed1f060b48c5e52fa42ff8def1b2

wined3d: Create CPU blitter also for GDI render.
Fixes a regression introduced in cad4badbcf25992e0c61521aa15e639c2611f5d6.

Signed-off-by: Sebastian Lackner <sebastian@fds-team.de>

dlls/wined3d/device.c
@@ -1190,6 +1190,15 @@ HRESULT CDECL wined3d_device_init_gdi(struct wined3d_device *device,
         goto err_out;
     }
     device->swapchains[0] = swapchain;
+
+    if (!(device->blitter = wined3d_cpu_blitter_create()))
+    {
+        ERR("Failed to create CPU blitter.\n");
+        HeapFree(GetProcessHeap(), 0, device->swapchains);
+        device->swapchain_count = 0;
+        goto err_out;
+    }
+
     return WINED3D_OK;
 
 err_out:
@@ -1277,6 +1286,8 @@ HRESULT CDECL wined3d_device_uninit_3d(struct wined3d_device *device)
 HRESULT CDECL wined3d_device_uninit_gdi(struct wined3d_device *device)
 {
     unsigned int i;
+
+    device->blitter->ops->blitter_destroy(device->blitter, NULL);
 
     for (i = 0; i < device->swapchain_count; ++i)
     {